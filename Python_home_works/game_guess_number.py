0eaff3ff0a124f115ef274eaf69982af2f3ef848
Gargoros <vargveresk@yandex.ru>
2020-12-18 07:57:17 +0300

    added solution code

--- Python_home_works/game_guess_number.py
+++ Python_home_works/game_guess_number.py
@@ -7,3 +7,23 @@
 # Подсказка:
 # для "загадывания" числа используйте модуль random: импортируйте его и для генерации
 # (загадывания) числа вызовите метод random.randint(1, 50).
+
+import random
+
+ai_num = random.randint(1, 50)
+#print(ai_int)
+game_raund = 6
+print("Guess a number from 1 to 50 ")
+
+while game_raund > 0:
+    player_num = int(input("\n Enter the hidden number "))
+    if player_num > ai_num:
+        print("The hidden number is less")
+        game_raund -= 1
+    elif player_num < ai_num:
+        print("The hidden number is greater")
+        game_raund -= 1
+    else:
+        game_raund = 0
+        print("\nYou win")
+print("Game Over")
