fcdc4647f0d260f79709774430622e46749a803f
Gargoros <vargveresk@yandex.ru>
2020-12-20 18:17:49 +0300

    refactor: rename vars for better readability

--- Python_home_works/game_Rock_Paper_Scissors.py
+++ Python_home_works/game_Rock_Paper_Scissors.py
@@ -10,34 +10,36 @@
 
 import random
 
-game_fig   = ["R", "S", "P"]
-ai_fig     = random.choice(game_fig)
-game_raund = 1
+game_items   = ["R", "S", "P"]
+ai_hand      = random.choice(game_items)
+game_raund   = 1
 print("\nLet's play rock, paper, scissors\n")
-print(game_fig)
-print(ai_fig)
+print(game_items)
+print(ai_hand)
 
 while True:
-    player_fig = str(input("Enter a short name for the figure "))
-    if player_fig == "R" or player_fig == "S" or player_fig == "P":
-        print(f"Player chose is {player_fig} and ai chose is {ai_fig} ")
+    player_hand = str(input("Enter a short name for the figure "))
+    if player_hand == "R" or player_hand == "S" or player_hand == "P":
+        print(f"Player chose is {player_hand} and ai chose is {ai_hand} ")
         break
-    elif player_fig == "replay":
+    elif player_hand == "replay":
         break
     else:
         print("It doesn't look like a short name for the figure") 
           
 while game_raund != 0 :
-    if player_fig == "R" and ai_fig == "S" or player_fig == "S" and ai_fig == "P" or player_fig == "P" and ai_fig == "R":
+    if player_hand == "R" and ai_hand == "S" or player_hand == "S" and ai_hand == "P" or player_hand == "P" and ai_hand == "R":
         game_raund -= 1
         print("PLAYER WIN")
-    elif ai_fig == "R" and player_fig == "S" or ai_fig == "S" and player_fig == "P" or ai_fig == "P" and player_fig == "R":
+    elif ai_hand == "R" and player_hand == "S" or ai_hand == "S" and player_hand == "P" or ai_hand == "P" and player_hand == "R":
         game_raund -= 1
         print("AI WIN")
-    elif player_fig == ai_fig:
+    else:
         print("DRAW")
         break
 print("GAME OVER")
+
+
 
     
         
