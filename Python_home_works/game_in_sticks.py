1541ca0ba1f38717b07d1dc39851ca7cb2607da5
Gargoros <vargveresk@yandex.ru>
2021-01-03 04:57:30 +0300

    feat: rename task, add simple logic

--- Python_home_works/game_in_sticks.py
+++ Python_home_works/game_in_sticks.py
@@ -22,5 +22,30 @@
 # а с тем количеством, которое введёт пользователь 
 # (может он хочет играть с 20-ю палочками?).
 
-def wands_game(pl1, pl2):
+def board_game_sticks(*args):
     pass
+
+# Приветсвие
+def greeting(name1 = "Player 1", name2 = "Player 2"):
+    name_1 = str(input("Player 1, enter your name, or your name will be Player 1 "))
+    name_2 = str(input("Player 2, enter your name, or your name will be Player 2 "))
+
+    while True:
+        if not name_1.split() and name_2.split():
+            print(f"Hello players {name_1} and {name_2}. Let's play a very old and simple game!")
+        else:
+            print(f"Hello players {name1} and {name2}. Let's play a very old and simple game!")
+            break
+
+# Правила игры
+def game_rules():
+    print('''    In the game "Sticks" 
+    you will need to demonstrate your ability to calculate moves in advance. 
+    There are twenty wooden sticks on the playing field in front of you. 
+    The players, in turn, will take one, two or three sticks 
+    (how much to take is up to the player). 
+    The one who took the last stick loses, 
+    so the goal of the game is to leave this stick to the opponent. 
+    Player 1 traditionally has the first move.''')
+
+#Ход игры
