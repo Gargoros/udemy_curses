27ad94e8805fbbe2786e4ab06bf4208230b3d8dc
Gargoros <vargveresk@yandex.ru>
2021-01-15 15:46:25 +0300

    feat: add basic logic and simple functions

--- Python_home_works/game_in_sticks.py
+++ Python_home_works/game_in_sticks.py
@@ -22,21 +22,30 @@
 # а с тем количеством, которое введёт пользователь 
 # (может он хочет играть с 20-ю палочками?).
 
-def board_game_sticks(*args):
-    pass
+
+#Переменные
+sticks = int(input("Enter the number of sticks in the game "))
+player_1_name = str(input("Player 1, enter your name, or your name will be Player 1 "))
+player_2_name = str(input("Player 2, enter your name, or your name will be Player 2 "))
 
 # Приветсвие
-def greeting(name1 = "Player 1", name2 = "Player 2"):
-    name_1 = str(input("Player 1, enter your name, or your name will be Player 1 "))
-    name_2 = str(input("Player 2, enter your name, or your name will be Player 2 "))
-
+def greeting(name1, name2):
+    '''
+    DOCSTRING: checks user input and if an empty entry is found, replaces it.
+    INPUT:     two inputs from users, with names.
+    OUTPUT:    get the input value and print a greeting to the console for users.
+    '''
     while True:
-        if not name_1.split() and name_2.split():
-            print(f"Hello players {name_1} and {name_2}. Let's play a very old and simple game!")
+        if not name1 or not name2:
+            name1 = "Player_1"
+            name2 = "Player_2"
+            print(f"Hello {name1} and {name2}. Let's play a very old and simple game!")
+            return name1, name2
         else:
-            print(f"Hello players {name1} and {name2}. Let's play a very old and simple game!")
-            break
-
+            print(f"Hello {name1} and {name2}. Let's play a very old and simple game!")
+            return name1, name2
+        
+players_names = greeting(player_1_name, player_2_name)
 # Правила игры
 def game_rules():
     print('''    In the game "Sticks" 
@@ -48,4 +57,45 @@ def game_rules():
     so the goal of the game is to leave this stick to the opponent. 
     Player 1 traditionally has the first move.''')
 
+#Палочки играков
+
+def select_sticks(player_select, stick, player_name):
+    '''
+    DOCSTRING: subtracts from a given occurrence, where parameters are given by users.
+    INPUT: values ​​to subtract, the name of the user who is executing the request and the number from which will be subtracted
+    OUTPUT: subtraction result
+    '''
+    global sticks
+    if player_select in range(1, 4):
+        print(f"{player_name} removes {player_select} sticks from the game")
+        sticks -= player_select
+        print(sticks)
+        return sticks
+    else:
+        print("Incorrect input, please try again")
+
 #Ход игры
+
+
+def board_game_sticks():
+    '''
+    DOCSTRING:
+    INPUT:
+    OUTPUT:
+    '''
+    greeting(player_1_name, player_2_name)
+    game_rules()
+    while sticks != 0:
+        pl1_select = int(input(f"{players_names[0]} Enter the number of sticks to remove "))
+        if select_sticks(pl1_select, sticks, player_1_name) < 4:
+            print("p1 win")
+            break
+        pl2_select = int(input(f"{players_names[1]} Enter the number of sticks to remove "))
+        if select_sticks(pl2_select, sticks, player_2_name) < 4:
+            print("p2 win")
+            break
+
+        
+
+
+board_game_sticks()
