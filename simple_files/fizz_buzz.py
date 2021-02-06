aee21fc56c1bc4bd60bd4c8e9963beef480b6793
Gargoros <vargveresk@yandex.ru>
2021-02-06 22:12:50 +0300

    added some new practical home tasks

---
+++ simple_files/fizz_buzz.py
@@ -0,0 +1,9 @@
+def get_reply(numbers):
+	if numbers % 3 == 0 and numbers % 5 == 0:
+		return "FizzBuzz"
+	elif numbers % 3 == 0:
+		return "Fizz"
+	elif numbers % 5 == 0:
+		return "Buzz"
+	else:
+		return ""
