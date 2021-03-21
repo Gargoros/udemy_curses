4cfd4dfeedd1504c7fd4664322020ae78b3f5758
Gargoros <vargveresk@yandex.ru>
2021-03-21 08:31:43 +0300

    refactor: fixed some bugs

--- Python_home_works/project_python/dragon.py
+++ Python_home_works/project_python/dragon.py
@@ -3,6 +3,8 @@
 import random
 import time
 
+friendly_dragon = random.randint(1, 2)
+
 def introduction():
     print("\nВы находитесь в землях, заселенных драконами.\n")
     time.sleep(2)
@@ -15,11 +17,9 @@ def introduction():
 
 
 def cave():
-    print("\nВ какую пещеру вы войдете?  (нажмите клавишу 1 или 2)\n")
-    time.sleep(2)
-
     while True:
-        player_select = input()
+        time.sleep(2)
+        player_select = input("\nВ какую пещеру вы войдете?  (нажмите клавишу 1 или 2)\n")
         try:
             player_select = int(player_select)
             if player_select not in range(1, 3):
@@ -30,20 +30,19 @@ def cave():
             print("\nЭто не похоже на цифру! Попробуй еще раз!\n")
     return player_select
 
-player_cave = cave()
-friendly_dragon = random.randint(1, 2)
-
 def cave_check():
+    player_cave = cave()
     if player_cave == friendly_dragon:
         print("\nВы приближаетесь к пещере...\nЕе темнота заставляет вас дрожать от страха...\n", end = "")
         time.sleep(2)
-        print("Большой дракон выпрыгивает перед вами! Он раскрывает свою пасть и...\n...моментально вас съедает!\n")
+        print("Большой дракон медленно выходит к вам на встречу! Смотря на вас приокрывает пасть...\n...и предлогает взять сокровище!\n")
         time.sleep(2)
     else:
         print("\nВы приближаетесь к пещере...\nЕе ослепительное свечение успокаивает вас...\n", end = "")
         time.sleep(2)
-        print("Большой дракон медленно выходит к вам на встречу! Смотря на вас приокрывает пасть...\n...и предлогает взять сокровище!\n")
+        print("Большой дракон выпрыгивает перед вами! Он раскрывает свою пасть и...\n...моментально вас съедает!\n")
         time.sleep(2)
+        
 
 def replay():
     print("\nПопытаете удачу еще раз? (да или нет)\n")
@@ -62,8 +61,14 @@ def replay():
 
 def dragon_lands():
     introduction()
-    cave()
     cave_check()
-    replay()
-    pass
+    if replay() == "да":
+        introduction()
+        cave()
+        cave_check()
+    if replay() == "нет":
+        exit
+
+dragon_lands()
+
 
